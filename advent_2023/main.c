76870c66d8d5d19ec4c399f54113145e3ed1b489
Jury Razumau <mail@razumau.net>
2023-12-01 15:03:45 +0100

    Initial Commit

---
+++ advent_2023/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  advent_2023
+//
+//  Created by Jury Razumau on 01/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
